30393586e3634b342c09e3c66a6258702deec125
Crhistian Ramirez <cramirez@four51.com>
2020-07-30 16:37:45 -0500

    page renderer updates

--- projects/lib/src/buyer/components/page-renderer/page-renderer.component.ts
+++ projects/lib/src/buyer/components/page-renderer/page-renderer.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { WidgetService } from '../../../shared/services/widget.service';
+import { Component, OnInit, Input, Renderer2, Inject } from '@angular/core';
+import { WidgetService } from 'projects/lib/src/shared/services/widget.service';
+import { Document } from '@ordercloud/headstart-sdk';
+import { Meta, Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
+import { PageContentDoc } from 'projects/lib/src/admin/models/page-content-doc.interface';
 
 @Component({
   selector: 'cms-page-renderer',
@@ -7,14 +11,59 @@ import { WidgetService } from '../../../shared/services/widget.service';
   styleUrls: ['./page-renderer.component.scss']
 })
 export class PageRendererComponent implements OnInit {
-  @Input() page: string;
-  pageContent: string;
+  @Input() pageDoc: Document;
+  content: string;
 
   constructor(
-    private widgetService: WidgetService
+    private widgetService: WidgetService,
+    private metaService: Meta,
+    private titleService: Title,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: HTMLDocument
   ) { }
 
   ngOnInit(): void {
-    this.pageContent = this.widgetService.applyDateRules(this.page);
+    const page: PageContentDoc = this.pageDoc.Doc;
+    this.content = this.widgetService.applyDateRules(page.Content);
+    this.setMetaData(page);
+    this.loadScripts(page.HeaderEmbeds, page.FooterEmbeds);
+  }
+
+  private setMetaData(page: PageContentDoc): void {
+    // addTag results in dupes, so we use updateTag instead
+
+    // normal metadata
+    this.titleService.setTitle(page.Title);
+    this.metaService.updateTag({ property: 'application-name', content: page.SiteUrl });
+    this.metaService.updateTag({ property: 'description', content: page.Description });
+
+    // open graph meta data
+    this.metaService.updateTag({property: 'og:type', content: 'website'});
+    this.metaService.updateTag({property: 'og:title', content: page.Title});
+    this.metaService.updateTag({property: 'og:description', content: page.Description});
+    this.metaService.updateTag({property: 'og:image', content: page.MetaImageUrl});
+
+    // twitter metadata
+    this.metaService.updateTag({ property: 'twitter:card', content: 'summary' });
+    this.metaService.updateTag({ property: 'twitter:site', content: page.SiteUrl });
+    this.metaService.updateTag({ property: 'twitter:title', content: page.Title });
+    this.metaService.updateTag({ property: 'twitter:description', content: page.Description });
+    this.metaService.updateTag({property: 'twitter:image', content: page.MetaImageUrl});
+  }
+
+  private loadScripts(headerEmbeds: string, footerEmbeds: string): void {
+    this.createScriptTag(headerEmbeds, 'head');
+    this.createScriptTag(footerEmbeds, 'body');
+  }
+
+  private createScriptTag(content: string, appendTo: string): void {
+    // create script
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.textContent = content;
+
+    // append to target element
+    const target = this.document.getElementsByTagName(appendTo)[0];
+    this.renderer.appendChild(target, script);
   }
 }
