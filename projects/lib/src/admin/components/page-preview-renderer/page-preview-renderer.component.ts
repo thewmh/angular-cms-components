42e95e7b9318fcf37a9e060b113f6c8a65ba1c74
Crhistian Ramirez <cramirez@four51.com>
2020-11-16 17:01:36 -0600

    WIN-1963 fix page preview bug

--- projects/lib/src/admin/components/page-preview-renderer/page-preview-renderer.component.ts
+++ projects/lib/src/admin/components/page-preview-renderer/page-preview-renderer.component.ts
@@ -36,7 +36,7 @@ export class PagePreviewRendererComponent implements OnInit {
       <head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      ${this.remoteCss ? `<style>@import url('${this.remoteCss}'); img { width: 100% !important; height: auto !important; } </style>` : ''}
+      ${this.remoteCss ? `<style>@import url('${this.remoteCss}'); img { max-width: 100% !important; height: auto !important; display: inline-block; } </style>` : ''}
       </head>
       <body style="padding-top:0 !important;">${this.html}</body>
       </html>`;
