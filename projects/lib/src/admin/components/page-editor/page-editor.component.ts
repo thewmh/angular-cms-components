59ce940b5911a8f39dc9ef925eac36fc7d88a764
cynthia <cricomendoza@four51.com>
2021-08-27 15:42:53 -0500

    add isWinmarkApp input and fix merge conflict errors

--- projects/lib/src/admin/components/page-editor/page-editor.component.ts
+++ projects/lib/src/admin/components/page-editor/page-editor.component.ts
@@ -62,6 +62,7 @@ export class PageEditorComponent implements OnInit, OnChanges {
   @Input() assetTypes: ASSET_TYPES[] = DEFAULT_ASSET_TYPES;
   @Input() additionalAssetFilters?: TemplateRef<any>;
   @Input() defaultListOptions?: ListArgs<Asset> = { filters: { Active: true } };
+  @Input() isWinmarkApp?: boolean = false;
   @Input() beforeAssetUpload?: (asset: AssetUpload) => Promise<AssetUpload>;
   @Input() beforeDocumentSave?: (page: Partial<PageContentDoc>) => Promise<Partial<PageContentDoc>>;
   @Output() selectedAssetChange = new EventEmitter<Asset | Asset[]>();
@@ -235,7 +236,7 @@ export class PageEditorComponent implements OnInit, OnChanges {
         }
       );
     } else {
-      updated = await ContentManagementClient.Documents.Create(this.pageSchemaID, {
+      const doc = {
         Doc: {
           ...this.page,
           Author: fullName,
